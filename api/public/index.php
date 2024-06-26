b91432ec24b4927d834f174c1a5a8cb03b333b25
d.potseluyev <d.potseluyev@onevision.kz>
2024-06-26 15:08:19 +0500

    Add: роутинг, DI, mw, конфиги

--- api/public/index.php
+++ api/public/index.php
@@ -2,26 +2,11 @@
 
 declare(strict_types=1);
 
-use App\Http\Action\HomeAction;
-use DI\ContainerBuilder;
-use Slim\Factory\AppFactory;
-
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new ContainerBuilder();
-$builder->addDefinitions([
-    'config' => [
-        'debug' => (bool) getenv('APP_DEBUG'),
-    ]
-]);
-$container = $builder->build();
+$container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-$app->addErrorMiddleware($container->get('config')['debug'], true, true);
-
-$app->get('/', HomeAction::class);
-
+$app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
